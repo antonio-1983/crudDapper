b2b918810d5524bad0ab074819a70a455258ab3c
Antonio Castro <72706037+antonio-1983@users.noreply.github.com>
2023-10-02 10:57:09 -0300

    commit de loa biblioteca

---
+++ DAOUsuario/DAOUsuarios.cs
@@ -0,0 +1,65 @@
+﻿
+using MySql.Data.MySqlClient;
+using Dapper;
+using MySqlX.XDevAPI.Common;
+
+namespace DAOUsuario
+
+{
+    public class DAOUsuarios
+    {
+        string connection = @"Server=localhost; Database=ejercicioDapper; Uid=root;";
+        string sqlUsuarios = "select Id,Nombre,Edad from Usuarios Where Activo=1;";
+        string sqlId = "select Id, Nombre, Edad from Usuarios where Id = @Id;";
+        string sqlInsert = "insert into Usuarios (Nombre,Edad) values (@Nombre,@Edad);";
+        string sqlDelete = "update Usuarios set Activo = 0 where id=@id;";
+
+        public DAOUsuarios()
+        {
+
+        }
+
+        public List<Usuario> ObtenerUsuarios()
+        {
+            using (var db = new MySqlConnection(connection))
+            {
+                List<Usuario> result = new List<Usuario>();
+                result= (List<Usuario>)db.Query<Usuario>(sqlUsuarios);
+                return result;
+            }
+            
+        }
+
+        public Usuario ObtenerUsuario(int id)
+        {
+            using (var db = new MySqlConnection(connection))
+            {
+               Usuario result = new Usuario();
+                result = db.QueryFirstOrDefault<Usuario>(sqlId, new { Id = 1 });
+                return result;
+            }
+        }
+
+        public void InsertarUsuario(string nombre, int edad) 
+        {
+            using (var db = new MySqlConnection(connection))
+            {
+                
+               var result = db.Execute(sqlInsert, new { nombre, edad });
+
+            }
+            
+        }
+
+        public void EliminarUsuario(int id)
+        {
+            using (var db = new MySqlConnection(connection))
+            {
+
+                var result = db.Execute(sqlDelete, new { id });
+
+            }
+
+        }
+    }
+}
