2bf34e19ce4288b72b9ea8e9d00d352ad272ff93
Antonio Castro <72706037+antonio-1983@users.noreply.github.com>
2023-10-18 10:50:25 -0300

    se agrego usuariosmanager

---
+++ UsuariosManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAOUsuario;
+using Ninject;
+using Org.BouncyCastle.Bcpg;
+
+namespace EjercicioDapperCRUD
+{
+    public class UsuariosManager
+    {
+        public IDAOUsuarios DAOUsuarios { get; private set; }
+        Ninject.IKernel kernel = new StandardKernel();
+        
+
+        public List<Usuario> usuarios = new List<Usuario>();
+        public List<String> usuariosNombres = new List<String>();
+        public UsuariosManager(IDAOUsuarios daoUsuarios)
+        {
+            this.DAOUsuarios = daoUsuarios;
+        }
+
+        public string ObtenerListadoDeUsuarios() 
+        {
+            usuarios= DAOUsuarios.ObtenerUsuarios();
+            IEnumerable<Usuario> usuariosOrdenados = usuarios.OrderBy(user=>user.Edad);
+            foreach (var usuario in usuariosOrdenados)
+            {
+                usuariosNombres.Add(usuario.Nombre);
+            }
+            var val = String.Join(",", usuariosNombres);
+            return val;
+        }
+    }
+}
